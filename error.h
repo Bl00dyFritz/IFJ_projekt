3256d82d148b549d2c79ddede3c2dc6174b556f5
Fleck012 <alexzikla@gmail.com>
2024-10-13 14:19:44 +0200

    Modified error.h

--- error.h
+++ error.h
@@ -7,19 +7,16 @@
 #ifndef ERROR_H
 #define ERROR_H
 
-typedef enum {
-    NO_ERROR,                               //Žádna chyba
-    LEXICAL_ERROR,                          //Chyba v programu v rámci lexikální analýzy (chybná struktura aktuálního lexému)
-    SYNTAX_ERROR,                           //Chyba v programu v rámci syntaktické analýzy (chybná syntaxe programu, chybějící hlavička, atp.)
-    SEMANTIC_UNDEF_ERROR,                   //Sémantická chyba v programu – nedefinovaná funkce či proměnná
-    SEMANTIC_WRONG_NUM_OF_PAR_ERROR,        //Sémantická chyba v programu – špatný počet/typ parametrů u volání funkce
-    SEMANTIC_REDEF_ERROR,                   //Sémantická chyba v programu – redefinice proměnné nebo funkce
-    SEMANTIC_RETURN_ERROR,                  //Chybějící/přebývající výraz v příkazu návratu z funkce
-    SEMANTIC_COMP_ERROR,                    //Sémantická chyba typové kompatibility v aritmetických, řetězcových a relačních výrazech
-    SEMANTIC_INFER_ERROR,                   //Sémantická chyba odvození typu – typ proměnné není uveden a nelze odvodit od použitého výrazu
-    SEMANTIC_UNUSED_VAR_ERROR,              //Sémantická chyba nevyužité proměnné v jejím rozsahu platnosti
-    SEMANTIC_OTHER_ERROR,                   //Ostatní sémantické chyby
-    INTERNAL_COMP_ERROR = 99                //Interní chyba překladače tj. neovlivněná vstupním programem
-} ERROR_CODES;
+#define LEXICAL_ERROR 1                      //Chyba v programu v rámci lexikální analýzy (chybná struktura aktuálního lexému)
+#define SYNTAX_ERROR 2                       //Chyba v programu v rámci syntaktické analýzy (chybná syntaxe programu, chybějící hlavička)
+#define SEMANTIC_UNDEF_ERROR 3               //Sémantická chyba v programu – nedefinovaná funkce či proměnná
+#define SEMANTIC_WRONG_NUM_OF_PAR_ERROR 4    //Sémantická chyba v programu – špatný počet/typ parametrů u volání funkce
+#define SEMANTIC_REDEF_ERROR 5               //Sémantická chyba v programu – redefinice proměnné nebo funkce
+#define SEMANTIC_RETURN_ERROR 6              //Chybějící/přebývající výraz v příkazu návratu z funkce
+#define SEMANTIC_COMP_ERROR 7                //Sémantická chyba typové kompatibility v aritmetických, řetězcových a relačních výrazech
+#define SEMANTIC_INFER_ERROR 8               //Sémantická chyba odvození typu – typ proměnné není uveden a nelze odvodit od použitého výrazu
+#define SEMANTIC_UNUSED_VAR_ERROR 9          //Sémantická chyba nevyužité proměnné v jejím rozsahu platnosti
+#define SEMANTIC_OTHER_ERROR 10              //Ostatní sémantické chyby
+#define INTERNAL_COMP_ERROR 99               //Interní chyba překladače tj. neovlivněná vstupním programem
 
 #endif /** ERROR_H **/
