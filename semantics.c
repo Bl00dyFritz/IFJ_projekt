a3ad1a72075ab2f1f1ccfdebf55e28c565a2b672
Nikola Jordanov <njordanov06@gmail.com>
2024-11-22 11:06:10 +0100

    Added files for the implmentation of the semantic analyzer

---
+++ semantics.c
@@ -0,0 +1,13 @@
+/**
+ * Implementace semanticke analyzy imperativniho jazyka IFJ24
+ * @file semantics.c
+ * @brief Implementace semanticke analyzy
+ * @author Nikola Jordanov xjordan00
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "semantics.h"
+#include "ast.h"
+#include "symtable.h"
+#include "error.h"
---
+++ semantics.h
@@ -0,0 +1,12 @@
+/**
+ * Implementace semanticke analyzy imperativniho jazyka IFJ24
+ * @file semantics.h
+ * @brief Hlavickovy soubor pro semantickou analyzu
+ * @author Nikola Jordanov xjordan00
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "ast.h"
+#include "symtable.h"
+#include "error.h"
